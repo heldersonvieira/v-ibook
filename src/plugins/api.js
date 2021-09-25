bec8504c23ac777fb75d65f402c799544783dec6
heldersonvieira <vieirahelder.fort@gmail.com>
2021-09-25 01:02:12 -0300

    Adicionado o plugin $get.allBook e $get.chosenBook

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -2,10 +2,15 @@ import connection from '@/services/axios';
 
 export default {
     install: (app) => {
-        app.config.globalProperties.$api = {
-            async get(path) {
-                const { data } = await connection.get(path);
-                return data;
+        app.config.globalProperties.$get = {
+            async allBooks() {
+                const { data: allBooks } = await connection.get('books');
+                return allBooks;
+            },
+
+            async chosenBook(id) {
+                const { data: book } = await connection.get(`books/${id}`);
+                return book;
             },
         };
     },
